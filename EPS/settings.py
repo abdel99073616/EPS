e606f25170d724bda76eb9c89653aaaf0abab37a
abdo <abdel99073616@gmail.com>
2021-04-14 14:44:03 +0200

    commit122

--- EPS/settings.py
+++ EPS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z0m%4tsv#unot47yv#h*oct*e2_j@-hngz-23k$(y84n#b)v-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokudjangoapp120.herokuapp.com' , '127.0.0.1']
 
 
 # Application definition
--- Procfile
+++
@@ -1,5 +0,0 @@
-release: python3 manage.py makemigrations --no-input
-release: python3 manage.py migrate --no-input
-
-web: gunicorn EPS.wsgi
-
