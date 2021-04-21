841a20b066279b91de3e3edcd15582d6ce973c47
abdo <abdel99073616@gmail.com>
2021-04-21 05:52:38 +0200

    backend Quiz

--- Model/admin.py
+++ Model/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Student)
-admin.site.register(Quiz)
+admin.site.register(Result)
+admin.site.register(Quiz_2)
+
+
+class AnswerInline (admin.TabularInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
+admin.site.register(Question , QuestionAdmin)
+admin.site.register(Answer)
--- Model/models.py
+++ Model/models.py
@@ -48,12 +48,53 @@ class Quiz(models.Model):
     def __str__(self):
         return self.Bady
 
+class Quiz_2(models.Model):
+    DIFF_CHOICE = (
+        ('easy', 'easy'),
+        ('medium', 'medium'),
+        ('hard', 'hard'),
+    )
+    name = models.CharField(max_length=5000)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="Quiz Time in minutes")
+    required_score_to_pass = models.IntegerField(help_text="Quiz degree to pass")
+    difficulty = models.CharField(max_length=20 , choices=DIFF_CHOICE)
+
+    def __str__(self):
+        return f"{self.name} - {self.topic} "
+
+    def get_questions(self):
+        return self.question_set.all()
+
+
+class Question(models.Model):
+    text = models.TextField(max_length=200)
+    quiz = models.ForeignKey(Quiz_2 , on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.text)
+
+    def get_answer(self):
+        return self.answer_set.all()
+
+class Answer(models.Model):
+    text = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question , on_delete=models.CASCADE )
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"question: {self.question.text} , answer: {self.text} , correct: {self.correct}"
 
 
 
+class Result(models.Model):
+    quiz = models.ForeignKey(Quiz_2 , on_delete=models.CASCADE)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    score = models.FloatField()
 
 
-
-
-
-
+    def __str__(self):
+        return str(self.pk)
